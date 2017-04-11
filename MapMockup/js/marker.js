ef84f623c06e30f8c9cb534a8579f364bc50ccb0
Aaron Taylor <halfnote1004@gmail.com>
2017-04-10 22:36:30 -0400

    Locations are now being read and turned into markers using json file and jquery.

--- MapMockup/js/marker.js
+++ MapMockup/js/marker.js
@@ -8,47 +8,38 @@ function initMap() {
   // creates a new map centered at the Rensselaer Union 
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 42.729948799999995, lng: -73.6766655},
-    zoom: 17,
+    zoom: 19,
     styles: [{"stylers":[{"hue": "#dd0d0d"}]},{"featureType": "road","elementType": "labels","stylers":[{"visibility":"off"}]},
     {"featureType":"road","elementType":"geometry","stylers":[{"lightness": 100},{"visibility":"simplified"}]}],
     mapTypeId: google.maps.MapTypeId.HYBRID
   });
+  
+ 
+  var largeInfowindow = new google.maps.InfoWindow();
 
   // Get all notable locations from a JSON file
   $(document).ready(function(){
     $.getJSON("json/locations.json", function(json) {
-      console.log(json); // this will show the info it in firebug console
+      $.each(json, function(index, location) {
+         // Get the position from the location object
+        var position = new google.maps.LatLng(location.latitude,location.longitude);
+        var title = location.title;
+        // Create a marker per location, and put into markers array.
+         var marker = new google.maps.Marker({
+          position: position,
+          title: title,
+          animation: google.maps.Animation.DROP,
+        });
+        // Push the marker to our array of markers.
+        markers.push(marker);
+        // Create an onclick event to open an infowindow at each marker.
+        marker.addListener('click', function() {
+          populateInfoWindow(this, largeInfowindow);
+        });
+      });
     });
   });
-  
-  // These are the real estate listings that will be shown to the user.
-  // Normally we'd have these in a database instead.
-  var locations = [
-    {title: 'Troy', location: {lat: 42.729948799999995, lng: -73.6766655}},
-    {title: '133_Sunset_Terrace', location: {lat: 42.72897, lng: -73.66867}},
-    {title: '15th_Street_Parking', location: {lat: 42.72856, lng: -73.6779}}
-  ];
 
-  var largeInfowindow = new google.maps.InfoWindow();
-  // The following group uses the location array to create an array of markers on initialize.
-  for (var i = 0; i < locations.length; i++) {
-    // Get the position from the location array.
-    var position = locations[i].location;
-    var title = locations[i].title;
-    // Create a marker per location, and put into markers array.
-     var marker = new google.maps.Marker({
-      position: position,
-      title: title,
-      animation: google.maps.Animation.DROP,
-      id: i
-    });
-    // Push the marker to our array of markers.
-    markers.push(marker);
-    // Create an onclick event to open an infowindow at each marker.
-    marker.addListener('click', function() {
-      populateInfoWindow(this, largeInfowindow);
-    });
-  }
   document.getElementById('show-listings').addEventListener('click', showListings);
   document.getElementById('hide-listings').addEventListener('click', hideListings);
 }
